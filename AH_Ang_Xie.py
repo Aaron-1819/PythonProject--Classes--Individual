0371a7a164455e71178db5e9778ea295a3456384
Aaron-1819 <xieang18@gmail.com>
2022-12-12 21:42:25 -0700

    update
    V1.0

--- AH_Ang_Xie.py
+++ AH_Ang_Xie.py
@@ -1,11 +1,13 @@
 #Author:Ang Xie
 
+#This is a class for doctors
 class Doctor:
     
     def __init__(self):
         pass
         
     def formatDrInfo(self,doctor_list):
+        #Formats each doctor’s information (properties) in the same format used in the .txt file 
         format_doctor=''
         for doctor in doctor_list:
             doctor=doctor[0]+'_'+doctor[1]+'_'+doctor[2]+'_'+doctor[3]+'_'+doctor[4]+'_'+doctor[5]+'\n'
@@ -13,8 +15,9 @@ class Doctor:
         return format_doctor
         
     def readDoctorsFile(self):
+        #Reads from “doctors.txt” file and fills the doctor objects in a list
         doctor_list=[]
-        with open ('Project Data/files/doctors.txt','r') as rf:
+        with open ('doctors.txt','r') as rf:
             for row in rf:
                 row = row.rstrip('\n')
                 row=row.split('_')
@@ -22,6 +25,7 @@ class Doctor:
         return doctor_list
     
     def addDrToFile(self,doctor_list):
+        # Writes new doctors to the doctors list.
         self.id=input('Enter the doctor’s ID:\n')
         self.name=input('Enter the doctor’s name:\n')
         self.speciality=input('Enter the doctor’s specility:\n')
@@ -33,12 +37,14 @@ class Doctor:
         return doctor_list
     
     def writeListOfDoctorsToFile(self,format_doctor):
-        with open('Project Data/files/doctors.txt','w') as wf:
+        # Writes the list of doctors to the doctors.txt file after formatting it correctly
+        with open('doctors.txt','w') as wf:
             wf.write(format_doctor)
         pass
     
     
     def searchDoctorById(self,doctor_list):
+        #Searches whether the doctor is in the list of doctors/file using the doctor ID that the user enters
         id= input('\nEnter the doctor’s ID:\n')
         for doctor in doctor_list:
             if id == doctor[0]:
@@ -49,6 +55,7 @@ class Doctor:
         return False
     
     def searchDoctorByName(self,doctor_list):
+        #Searches whether the doctor is in the list of doctors/file using the doctor name that the user enters
         name=input('\nEnter the doctor’s name:\n\n')
         for doctor in doctor_list:
             if name == doctor[1]:
@@ -59,6 +66,7 @@ class Doctor:
         return False
         
     def editDoctorInfo(self,doctor_list):
+        #Asks the user to enter the ID of the doctor to change their information, and then the user can enter the new doctor information
         id= input('\nPlease enter the id of the doctor that you want to edit their information:\n\n')
         for doctor in doctor_list:
             if id == doctor[0]:
@@ -74,13 +82,14 @@ class Doctor:
         pass
     
     def displayDoctorsList(self,doctor_list):
+        #Displays doctor information on different lines
         for doctor in doctor_list:
             print(doctor[0].ljust(15),doctor[1].ljust(15),doctor[2].ljust(15),doctor[3].ljust(15),doctor[4].ljust(15),doctor[5].ljust(15))
         pass
         
-
+#The management process of doctor
 def doctors():
-    for i in range(1000000):
+    while True:
         dt=Doctor()
         doctor_list=dt.readDoctorsFile()
         nav2=input('\nDoctors Menu:\n1 - Display Doctors list\n2 - Search for doctor by ID\n3 - Search for doctor by name\n4 - Add doctor\n5 - Edit doctor info\n6 - Back to the Main Menu\n')
@@ -109,17 +118,16 @@ def doctors():
         else:
             print('\nYou input a wrong number. Please try it again.\n')
             print('\nBack to the prevoius Menu\n')
+            break
 
-                
+        
+        
+#Running main program
 if __name__ == '__main__':
-    for i in range(1000000):
+    while True:
         print('\nWelcome to Alberta Hospital (AH) Managment system\n')
         nav1=input("Select from the following options, or select 0 to stop: \n1 - 	Doctors \n2 - 	Facilities \n3 - 	Laboratories \n4 - 	Patients\n")
         if nav1=='1':
             doctors()
         if nav1=='0':
             break
-        
-        
-
-    
